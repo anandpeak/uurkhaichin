af4c63e6420dda76b7ed68bcf2dd8b8705701ed0
panda-desu <zr.freeze05@gmail.com>
2024-12-26 15:04:27 +0800

    added

--- src/components/Ending.jsx
+++ src/components/Ending.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { IoIosArrowRoundForward, IoMdArrowForward } from "react-icons/io";
+import "../style/animations.css";
 
 const Ending = () => {
+  const [showEndings, setShowEndings] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowEndings(true);
+    }, 2000); // 2 seconds delay
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []);
+
   return (
     <div className="text-center mb-[180px]">
       <div className="flex justify-center">
@@ -10,42 +20,45 @@ const Ending = () => {
         </div>
       </div>
       <div className="mt-6">
-        <p className="text-[#1A1A1A] text-[22px]  font-semibold mb-6">
+        <p className="text-[#1A1A1A] text-[22px] font-semibold mb-6">
           Идэвхитэй хариулсанд баяр хүргэе!
         </p>
         <p className="text-[#1A1A1A] text-sm">
-          Танд тун удахгүй мейлээр ажлын санал очих болно.
+          Танд тун удахгүй мейлээр мэдээлэл очих болно оо.
         </p>
       </div>
-      <div className="mt-6">
-        <p className="font-bold mb-2">
-          Та AI ашиглан өөрийн зургаа доор үзүүлсэн шиг зуруулах боломжтой шүү.
-        </p>
-        <div className="flex items-center justify-between gap-1">
-          <div className="w-[150px] h-[200px] rounded-xl border-4 border-[#fff] border-opacity-80">
-            <img
-              className="w-full h-full object-cover rounded-xl"
-              src="/img/example.svg"
-              alt="pfp"
-            />
-          </div>
-          <IoIosArrowRoundForward className="text-xl font-bold" />
+      {showEndings && (
+        <div className="mt-6 endings">
+          <p className="font-bold mb-2">
+            Та AI ашиглан өөрийн зургаа доор үзүүлсэн шиг зуруулах боломжтой
+            шүү.
+          </p>
+          <div className="flex items-center justify-between gap-1">
+            <div className="w-[150px] h-[200px] rounded-xl border-4 border-[#fff] border-opacity-80">
+              <img
+                className="w-full h-full object-cover rounded-xl"
+                src="/img/example.svg"
+                alt="pfp"
+              />
+            </div>
+            <IoIosArrowRoundForward className="text-xl font-bold" />
 
-          <div className="w-[150px] h-[200px] rounded-xl border-4 border-[#fff] border-opacity-80">
-            <img
-              className="w-full h-full object-cover rounded-xl"
-              src="/img/example.png"
-              alt="pfp"
-            />
+            <div className="w-[150px] h-[200px] rounded-xl border-4 border-[#fff] border-opacity-80">
+              <img
+                className="w-full h-full object-cover rounded-xl"
+                src="/img/example.png"
+                alt="pfp"
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-center mt-4">
+            <button className="w-[280px] bg-[#fff] rounded-xl text-[#1E293B] p-3 flex items-center justify-center gap-2 font-bold">
+              Зурагтай болох
+              <IoMdArrowForward />
+            </button>
           </div>
         </div>
-        <div className="flex items-center justify-center mt-4">
-          <button className="w-[280px] bg-[#fff] rounded-xl text-[#1E293B] p-3 flex items-center justify-center gap-2 font-bold">
-            Зурагтай болох
-            <IoMdArrowForward />
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
